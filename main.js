10e0aef121205431481bc6e1820aaba35e70efc1
Doubzero <131899921+Doubzero@users.noreply.github.com>
2023-06-19 15:01:23 -0400

    Change size button completed.

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-const container = document.getElementById("container");
-const eraseButton = document.getElementById("eraseBtn");
+const DEFAULT_SIZE = 16;
+let currentSize = DEFAULT_SIZE;
 
-eraseButton.onclick = () => eraseGrid();
+const container = document.getElementById("gridlayout");
+const eraseButton = document.getElementById("resetBtn");
+const changeSizeButton = document.getElementById("selectsizebtn");
 
-function createGrid() {
-  for (let i = 0; i < 256; i++) {
+eraseButton.onclick = () => reloadGrid();
+changeSizeButton.onclick = () => changeSize();
+
+function createGrid(size) {
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}), 1fr`;
+  for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-cell");
     gridElement.addEventListener("mouseover", colorChange);
     container.appendChild(gridElement);
+  }
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+}
+
+function changeSize() {
+  const size = prompt("Change size (max 100)");
+  if (size > 100) {
+    alert("cannot be greater than 100. Setting Default size of 16");
+    reloadGrid();
+  } else {
+    setCurrentSize(size);
+    reloadGrid();
   }
 }
 
@@ -17,8 +39,16 @@ function colorChange(e) {
     e.target.style.backgroundColor = "black";
   }
 }
+
+function reloadGrid() {
+  eraseGrid();
+  createGrid(currentSize);
+}
+
 function eraseGrid() {
   container.innerHTML = "";
-  createGrid();
 }
-createGrid();
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+};
